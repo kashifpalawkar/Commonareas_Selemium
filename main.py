5d393bb6c0efef21c2d3553ffc9b6c7694f60f4c
muddey19 <muddey2019@gmail.com>
2022-03-17 00:39:07 +0530

    Commit

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
-from pandas import pd
+import pandas as pd
+import os
 #commit
 
 df3 = pd.read_excel(r'C:/Users/kashif/Desktop/websocket/aatrades.xlsx')
 PATH = "C:\\Users\\Kashif\\Music\\chromedriver\\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
+
+# get current directory
+path = os.getcwd()
+print("Current Directory", path)
 driver.get("https://www.youtube.com/shorts/s8wEGKkNNv0")
 #driver.close()
 
