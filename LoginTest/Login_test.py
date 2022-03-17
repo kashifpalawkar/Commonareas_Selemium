5288b78dd5a0d35019e785e92d86b1fc5af47210
muddey19 <muddey2019@gmail.com>
2022-03-17 21:23:21 +0530

    New changes

--- LoginTest/Login_test.py
+++ LoginTest/Login_test.py
@@ -1,3 +1,25 @@
-class LoginTest():
+class Login:
 
-    def LoginTest(Username, Password):
+
+    def __init__(self,Rrow,URL):
+
+     
+     self.Rrow = Rrow
+     self.URL= URL
+
+
+    def loginToApp(self):
+
+        rrow = self.Rrow
+        URL = self.URL
+
+        print (rrow)
+       #for index, row in df1.iterrows():
+        if rrow.loc["TestCase"] == 'Login to Site':
+            print ("kitmoduleFound")
+            TestCase = rrow.loc["TestCase"]
+            Username = rrow.loc["Parameter1"]
+            Password = rrow.loc["Parameter2"]
+            print(TestCase,Username,Password,URL)
+            #kititem = kt(df1,df2)
+            #continue;
--- main.py
+++ main.py
@@ -3,57 +3,58 @@ from inspect import Parameter
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
-<<<<<<< HEAD
-import pandas as pd
-import os
-#commit
-=======
 from time import sleep
 from datetime import timedelta
 import pandas as pd
 import xlwt
 import os
+import LoginTest.Login_test as Login_Test
 
 
+dff2 = pd.DataFrame()
+row = 0
 # get current directory
+baseurl = 'https://www.phptravels.net/login'
 path = os.getcwd()
 excelFile = path + '\\TestData\\testdata.xlsx'
 df3 = pd.read_excel(excelFile)
-
 for index,row in df3.iterrows():
         executeValue = str(df3.loc[index,'Execute'])
         if (executeValue.lower() == "Yes".lower()):
             parameters = df3.loc[index,:]
             funcName = parameters['FunctionName'] 
             print(funcName)
+            if (funcName == 'LoginTest'):
+                loginfunc = Login_Test.Login(parameters,baseurl)
+                login_call = loginfunc.loginToApp()
+                print(login_call)
+        elif(executeValue.lower() == "No".lower()):
+            parameters = df3.loc[index,:]
+            #funcName = parameters['FunctionName']
+            #Execute = parameters['Execute']
+            #TestCase = parameters['TestCase']
+            #Parameter1 = parameters['Parameter1']
+            noRunxl = pd.DataFrame()
+            #noRunxl.loc[rrow,'Testcase'] = TestCase
+            dff2 = dff2.append(parameters, ignore_index=False)
+            print(dff2)
+            ResultexcelFile = path + '\\TestData\\Results.xlsx'
+            dff2.to_excel(ResultexcelFile)
+        
+            
             
 
 driver = webdriver.Chrome()
 
-#options = webdriver.ChromeOptions()
-#options.add_experimental_option('excludeSwitches', ['enable-logging'])
-#driver = webdriver.Chrome(options=options)
-driver.get("https://tm.ca-test.com")
-sleep(5)
+
+driver.get(baseurl)
+sleep(120)
 driver.close()
 driver.quit()
 
-
->>>>>>> 7e44758dc8f5b374803e1c935a36de2e9109950a
-
-
-<<<<<<< HEAD
-
-# get current directory
-path = os.getcwd()
-print("Current Directory", path)
-driver.get("https://www.youtube.com/shorts/s8wEGKkNNv0")
-#driver.close()
-=======
->>>>>>> 7e44758dc8f5b374803e1c935a36de2e9109950a
-
-
-
+#options = webdriver.ChromeOptions()
+#options.add_experimental_option('excludeSwitches', ['enable-logging'])
+#driver = webdriver.Chrome(options=options)
 
 #driver = webdriver.Firefox(executable_path='C:\\Users\\Kashif\\Music\\geckodriver-v0.26.0-win64')
 #driver = webdriver.Firefox(firefox_binary=binary,executable_path="C:\\Users\\Kashif\\Music\\geckodriver\\geckodriver.exe")
